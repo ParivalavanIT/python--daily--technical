1f1c1855b144a32f1104697f8f5b03d2d26f9012
ParivalavanIT <112223851+ParivalavanIT@users.noreply.github.com>
2023-08-30 13:35:54 +0530

    commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-def fizzbuzz(i):
+def fizzbuzz(i,j):
     if i%3==0 and i%5==0:
         print ("Fizz Buzz")
     elif i%3==0 :
@@ -8,10 +8,10 @@ def fizzbuzz(i):
     else:
         print(i)
      
-    if i!=100 :
+    if i!=j :
         return fizzbuzz(i+1)
     else:
         return
     
     
-fizzbuzz(1)
+fizzbuzz(1,100)
