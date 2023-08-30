f254ebdbe90c1b3a2c90f08c17edaf0ae185f7e3
ParivalavanIT <112223851+ParivalavanIT@users.noreply.github.com>
2023-08-30 13:39:43 +0530

    commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,14 @@
 def fizzbuzz(start,end):
-    if start%3==0 and start%5==0:
+    if start % 3 == 0 and start % 5 == 0 :
         print ("Fizz Buzz")
-    elif start%3==0 :
+    elif start % 3 == 0 :
         print ("Fizz")
-    elif start%5==0 :
+    elif start % 5 == 0 :
         print ("Buzz")   
     else:
         print(start)
      
-    if start!=end :
+    if start != end :
         return fizzbuzz(start+1,end)
     else:
         return
