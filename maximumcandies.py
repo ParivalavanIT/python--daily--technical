deb3b386924ef877614323a443a90b531a86b9ba
ParivalavanIT <112223851+ParivalavanIT@users.noreply.github.com>
2023-09-01 14:24:37 +0530

    commit

--- maximumcandies.py
+++ maximumcandies.py
@@ -1,5 +1,6 @@
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+from typing import List
+
+def kidsWithCandies( candies: List[int], extraCandies: int) -> List[bool]:
         maxCandies = max(candies)
         result = []
         for i in range(len(candies)):
@@ -8,3 +9,8 @@ class Solution:
             else:
                 result.append(False)
         return result
+
+
+list=[3,7,4,9,2,1]
+candies_extra=4
+print(kidsWithCandies(list,candies_extra))
